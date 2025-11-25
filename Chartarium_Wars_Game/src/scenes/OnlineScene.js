3b5ab5db392aa23169c3510f7b0bbf392be44088
Chyarlos <144938655+Chyarlos@users.noreply.github.com>
2025-11-25 23:55:14 +0100

    Añadida la escena de Online e indicado que no va a estar disponible en esta versión

---
+++ Chartarium_Wars_Game/src/scenes/OnlineScene.js
@@ -0,0 +1,44 @@
+import Phaser from 'phaser';
+
+
+export class OnlineScene extends Phaser.Scene {
+    constructor() {
+        super('OnlineScene');
+    }
+
+    preload() {//carga de archivos
+        this.load.image('Fondo', 'imagenes/Fondo.jpg');
+    }
+
+    create() {
+
+        //fondo
+        this.add.image(400, 310, 'Fondo');
+        
+        this.add.text(400, 100, 'Online Multiplayer', {
+            fontSize: '55px',
+            color: '#000000ff',
+            fontStyle: 'bold'
+        }).setOrigin(0.5);
+
+        this.add.text(400, 310, 'La funcionalidad online no está disponible en esta versión', {
+            fontSize: '22px',
+            color: '#000000ff',
+            fontStyle: 'bold'
+        }).setOrigin(0.5);
+        
+        
+        const localBtn = this.add.text(400, 520, 'Back to menu', {
+            fontSize: '50px',
+            color: '#000000ff',
+            fontStyle: 'bold'
+        }).setOrigin(0.5)
+        .setInteractive({useHandCursor: true})
+        .on('pointerover', () => localBtn.setColor('#535353ff'))
+        .on('pointerout', () => localBtn.setColor('#000000ff'))
+        .on('pointerdown', () => {
+            this.scene.start('MenuScene');
+        });
+
+    }
+}
