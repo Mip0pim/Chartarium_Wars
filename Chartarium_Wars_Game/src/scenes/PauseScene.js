9a1a8849cd4d1707f44b71f379706e10fc42d10d
Chyarlos <144938655+Chyarlos@users.noreply.github.com>
2025-11-25 23:52:49 +0100

    Modificado el aspecto de la escena de pausa para hacerlo más bonito

--- Chartarium_Wars_Game/src/scenes/PauseScene.js
+++ Chartarium_Wars_Game/src/scenes/PauseScene.js
@@ -10,17 +10,19 @@ export class PauseScene extends Phaser.Scene {
         this.add.rectangle(400, 300, 800, 600, 0x000000, 0.7);
 
         this.add.text(400, 200, 'Game Paused', {
-            fontSize: '64px',
-            color: '#ffffff'
+            fontSize: '65px',
+            color: '#bdbdbdff',
+            fontStyle: 'bold'
         }).setOrigin(0.5);
-
+        
         const resumeBtn = this.add.text(400, 320, 'Resume', {
             fontSize: '32px',
-            color: '#00ff00',
+            color: '#036c03ff',
+            fontStyle: 'bold'
         }).setOrigin(0.5)
         .setInteractive({useHandCursor: true})
-        .on('pointover', () => resumeBtn.setColor('#00ff88'))
-        .on('pointerout', () => resumeBtn.setColor('#00ff00'))
+        .on('pointover', () => resumeBtn.setColor('#002405ff'))
+        .on('pointerout', () => resumeBtn.setColor('#036c03ff'))
         .on('pointerdown', () => {
             this.scene.stop();
             this.scene.resume(data.originalScene);
@@ -29,11 +31,12 @@ export class PauseScene extends Phaser.Scene {
 
         const menuBtn = this.add.text(400, 400, 'Return to Main Menu', {
             fontSize: '32px',
-            color: '#ffffff',
+            color: '#a30303ff',
+            fontStyle: 'bold'
         }).setOrigin(0.5)
         .setInteractive({ useHandCursor: true })
-        .on('pointerover', () => menuBtn.setColor('#ff8888'))
-        .on('pointerout', () => menuBtn.setColor('#ffffff'))
+        .on('pointerover', () => menuBtn.setColor('#790000ff'))
+        .on('pointerout', () => menuBtn.setColor('#a30303ff'))
         .on('pointerdown', () => {
             this.scene.stop(data.originalScene);
             this.scene.start('MenuScene');
