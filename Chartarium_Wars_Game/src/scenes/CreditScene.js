672ae7d32920836d818487176be869ab8beec330
Chyarlos <144938655+Chyarlos@users.noreply.github.com>
2025-11-25 23:49:38 +0100

    Actualizado la escena de créditos para poner al equipo y modificar otras cosas

--- Chartarium_Wars_Game/src/scenes/CreditScene.js
+++ Chartarium_Wars_Game/src/scenes/CreditScene.js
@@ -8,6 +8,7 @@ export class CreditScene extends Phaser.Scene {
 
     preload() {//carga de archivos
         this.load.image('Fondo', 'imagenes/Fondo.jpg');
+        this.load.image('BTNCredit', 'imagenes/CreditosCW.png');
     }
 
     create() {
@@ -15,18 +16,45 @@ export class CreditScene extends Phaser.Scene {
         //fondo
         this.add.image(400, 310, 'Fondo');
         
-        this.add.text(400, 100, 'CREDITS', {
-            fontSize: '64px',
-            color: '#00ffff'
+        const creditBtn = this.add.image(400, 90, "BTNCredit")
+            .setOrigin(0.5);
+                creditBtn.setScale(1.5);
+        
+        this.add.text(400, 200, 'Creado por:', {
+            fontSize: '40px',
+            color: '#000000ff',
+            fontStyle: 'bold'
         }).setOrigin(0.5);
 
-        const localBtn = this.add.text(400, 320, 'Back to menu', {
-            fontSize: '24px',
-            color: '#00ff00',
+        this.add.text(400, 260, 'Carlos Martínez García', {
+            fontSize: '35px',
+            color: '#000000ff'
+        }).setOrigin(0.5);
+
+        this.add.text(400, 320, 'Pablo Romero Labrado', {
+            fontSize: '35px',
+            color: '#000000ff'
+        }).setOrigin(0.5);
+
+        this.add.text(400, 380, 'Iván Palacios Martín', {
+            fontSize: '35px',
+            color: '#000000ff'
+        }).setOrigin(0.5);
+
+        this.add.text(400, 440, 'Alex Yu Weng', {
+            fontSize: '35px',
+            color: '#000000ff'
+        }).setOrigin(0.5);
+
+        
+        const localBtn = this.add.text(400, 520, 'Back to menu', {
+            fontSize: '50px',
+            color: '#000000ff',
+            fontStyle: 'bold'
         }).setOrigin(0.5)
         .setInteractive({useHandCursor: true})
-        .on('pointerover', () => localBtn.setColor('#00ff88'))
-        .on('pointerout', () => localBtn.setColor('#00ff00'))
+        .on('pointerover', () => localBtn.setColor('#535353ff'))
+        .on('pointerout', () => localBtn.setColor('#000000ff'))
         .on('pointerdown', () => {
             this.scene.start('MenuScene');
         });
